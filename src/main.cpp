d3966fe459c7714af26589a0d561f6b689c205c3
ALLE MONNE Jonah <jallemonne@g-a207-21.intra.gobelins.fr>
2021-05-05 16:55:35 +0200

    wip

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,25 @@
 #include <CoreMIDI/MIDIServices.h>
 #include <CoreFoundation/CFRunLoop.h>
 
+MIDIPortRef     gOutPort = NULL;
+MIDIEndpointRef gDest = NULL;
+int             gChannel = 0;
+
 int main() {
 
     std::cout << "All is ok" << std::endl;
 
     // create client and ports
     MIDIClientRef client = NULL;
-    // MIDIClientCreate(CFSTR("MIDI Echo"), NULL, NULL, &client);
+    MIDIClientCreate(CFSTR("MIDI Echo"), NULL, NULL, &client);
+
+    MIDIPortRef inPort = NULL;
+    MIDIInputPortCreate(client, CFSTR("Input port"), MyReadProc, NULL, &inPort);
+    MIDIOutputPortCreate(client, CFSTR("Output port"), &gOutPort);
+
+    MIDIEndpointRef epRef = MIDIGetSource(1);
+
+
 
     int n = MIDIGetNumberOfDevices();
     std::cout << n << std::endl;
