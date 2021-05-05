1eccbfdd3554b90560cd1595a9a27bc2e4c53851
ALLE MONNE Jonah <jallemonne@g-a207-21.intra.gobelins.fr>
2021-05-05 16:27:10 +0200

    feat(core): link CoreMIDI

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,17 @@
 #include <iostream>
+#include <CoreMIDI/MIDIServices.h>
+#include <CoreFoundation/CFRunLoop.h>
 
 int main() {
 
     std::cout << "All is ok" << std::endl;
+
+    // create client and ports
+    MIDIClientRef client = NULL;
+    // MIDIClientCreate(CFSTR("MIDI Echo"), NULL, NULL, &client);
+
+    int n = MIDIGetNumberOfDevices();
+    std::cout << n << std::endl;
 
     return 0;
 }
