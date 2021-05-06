3a09249b848f94f0039deabd0628b14b8a6e6425
ALLE MONNE Jonah <jallemonne@g-a207-21.intra.gobelins.fr>
2021-05-06 15:45:10 +0200

    feat(core): play midi note

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,6 @@ int main() {
     std::cout << "All is ok" << std::endl;
     RtMidiOut *midiout = 0;
 
-
     // RtMidiOut constructor
     try {
         midiout = new RtMidiOut();
@@ -30,15 +29,47 @@ int main() {
         portName = midiout->getPortName(i);
         }
         catch (RtMidiError &error) {
-        error.printMessage();
-        goto cleanup;
+            error.printMessage();
+            // goto cleanup;
+            delete midiout;
+            exit(EXIT_FAILURE);
         }
         std::cout << "  Output Port #" << i+1 << ": " << portName << '\n';
     }
     std::cout << '\n';
 
-    cleanup:
-        delete midiout;
+    std::vector<unsigned char> message;
+    // Note On: 144, 64, 90
+    message.push_back(144);
+    message.push_back(64);
+    message.push_back(90);
+
+    unsigned int i = 1;
+    
+    midiout->openPort( 0 );
+    
+    while (1)
+    {
+
+        message[0] = 144;
+        message[1] = i;
+        midiout->sendMessage( &message );
+        i = (i + 1) % 128;
+
+        // std::cout << i << std::endl;
+        // std::cout <<  message[1] << std::endl;
+
+        sleep(1);
+        // std::cout << "midi sent" << std::endl;
+        // // Note Off: 128, 64, 40
+        message[0] = 128;
+        // message[1] = 64;
+        // message[2] = 40;
+        midiout->sendMessage( &message );
+    }
+    
+
+    delete midiout;
 
     return 0;
 }
